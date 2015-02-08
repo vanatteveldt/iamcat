946d54d26cff7d8cd3a2b9befe78e00b003c2258
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-02-08 16:28:18 +0100

    put all configs in upstart file

--- iamcat.py
+++ iamcat.py
@@ -1,19 +1,20 @@
 import os, os.path, glob
 from mako.template import Template
 
-IMAGES_DIR = "cats" # relative to DOCUMENT_ROOT
-
 DOCUMENT_ROOT = os.environ['DOCUMENT_ROOT']
+IMAGES_DIR = os.environ['IMAGES_DIR']
+TITLE = os.environ['TITLE']
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE = Template(filename=os.path.join(SCRIPT_DIR, 'template.html'))
 
-FILE_PATTERN = os.path.join(DOCUMENT_ROOT, IMAGES_DIR, "*")
+FILE_PATTERN = os.path.join(IMAGES_DIR, "*")
+
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
 
-    title = "I am CAT"
+    print IMAGES_DIR, FILE_PATTERN
 
     files = [f for f in glob.glob(FILE_PATTERN)
              if os.path.isfile(f)
@@ -22,6 +23,6 @@ def application(env, start_response):
     files.sort(key = lambda f: os.path.getmtime(f), reverse=True)
     files = [os.path.relpath(f, DOCUMENT_ROOT) for f in files]
 
-    return [TEMPLATE.render(**locals()).encode("utf-8")]
+    return [TEMPLATE.render(title=TITLE, files=files).encode("utf-8")]
 
 
